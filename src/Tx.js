8f11d7611167b08bd40c2d781ad5705a6726c01a
emi <emiliorizzo@gmail.com>
2019-03-23 07:31:26 +0000

    Update Tx, validate 'from' if present

--- src/Tx.js
+++ src/Tx.js
@@ -4,9 +4,11 @@ const { isValidAddress } = require('ethereumjs-util')
 const { gtZero } = require('./utils')
 
 function Tx (txData) {
-  const { to, nonce, gasPrice, gasLimit, chainId, data } = txData
+  const { from, to, nonce, gasPrice, gasLimit, chainId, data } = txData
   if (undefined === data) txData.data = ''
   if (!isValidAddress(to)) throw new TypeError(`Invalid to: ${to}`)
+  // validate from if exists
+  if (from && !isValidAddress(from)) throw new TypeError(`Invalid from: ${from}`)
   if (undefined === nonce) throw new TypeError('Invalid Nonce')
   if (!gtZero(chainId)) throw new TypeError('Invalid chainId')
   if (!gasPrice) throw new TypeError('Invalid gasPrice')
--- test/tx.spec.js
+++ test/tx.spec.js
@@ -1,31 +1,42 @@
 const Tx = require('../src/Tx')
 const { expect } = require('chai')
-const txData = {
-  to: '0x8a42fa7efeb939bf189fe4758377643d633f446f',
-  nonce: '0x01',
-  gasPrice: '0x001',
-  gasLimit: '0x200',
-  chainId: 31
+const txData = (props = {}) => {
+  return Object.assign({
+    to: '0x8a42fa7efeb939bf189fe4758377643d633f446f',
+    nonce: '0x01',
+    gasPrice: '0x001',
+    gasLimit: '0x200',
+    chainId: 31
+  }, props)
 }
 
 describe(`# Tx`, () => {
   // normal use
   it(`should return a Transaction instance`, () => {
-    let tx = Tx(txData)
+    let tx = Tx(txData())
     expect(typeof tx).to.be.equal('object')
     expect(tx.constructor.name).to.be.equal('Transaction')
   })
 
   // required values
-  objectRemoveOneProperty(txData).forEach(txd => {
+  objectRemoveOneProperty(txData()).forEach(txd => {
     it(`throw TypeError if missed a required value`, () => {
       expect(() => Tx(txd)).to.throw(TypeError)
     })
   })
 
   it(`throw TypeError if 'to' is an invalid address`, () => {
-    expect(() => Tx(Object.assign(txData, { to: '0x0434343434' }))).to.throw(TypeError)
+    expect(() => Tx(txData({ to: '0x0434343434' }))).to.throw(TypeError)
   })
+
+  it(`allow valid 'from' `, () => {
+    expect(() => Tx(txData({ from: '0x8a42fa7efeb939bf189fe4758377643d633f446f' }))).to.not.throw()
+  })
+
+  it(`throw TypeError if 'from' is an invalid address`, () => {
+    expect(() => Tx(txData({ from: '0x0434343434' }))).to.throw(TypeError)
+  })
+
 })
 
 function objectRemoveOneProperty (obj) {
